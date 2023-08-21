4ce5be185016e18e804b83fd33dc382901d38fc2
Dhiraj Basnet <dhirajbasnet@Dhirajs-MBP.attlocal.net>
2023-08-21 01:01:01 -0500

    added models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    inventory = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.title
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField()
+    bookingDate = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.name

9eef368b8be7c25eedb4a69bc9e5b1acc2b91784
Dhiraj Basnet <dhirajbasnet@Dhirajs-MBP.attlocal.net>
2023-08-21 22:13:38 -0500

    added serializers and urls for booking and menu items

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Booking,Menu
+from rest_framework.response import Response
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 def index(request):
     return render(request,'index.html',{})
+
+class MenuItemView(ListCreateAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    serializer_class = MenuSerializer
+    queryset = Menu.objects.all()
+
+class BookingViewSet(ModelViewSet):
+    serializer_class = BookingSerializer
+    queryset = Booking.objects.all()
